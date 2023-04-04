22d539b22a95b532d906413fa59b39565f01f814
rilwanulhuda <rilwanulhuda.dev@gmail.com>
2023-04-05 04:26:11 +0700

    add new class and fix deployment target

---
+++ OnlineFramework/SecondClass.swift
@@ -0,0 +1,16 @@
+//
+//  SecondClass.swift
+//  OnlineFramework
+//
+//  Created by Rilwanul Huda on 05/04/23.
+//
+
+import Foundation
+
+public class SecondClass {
+    public init() {}
+    
+    public func secondClassFunction() {
+        print("WAHAHAHHAHAHAHAHAHAHHAHAHAHHAHHAHAHAHA")
+    }
+}
